1cfce332ce9e2165327467a611e63a22fc118bb3
LorenzoSassia <167799289+LorenzoSassia@users.noreply.github.com>
2024-07-01 15:08:43 -0300

    Add files via upload

---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+/* Definimos las constantes de conexion */
+define('DB_HOST', 'localhost'); // El servidor MySQL
+define('DB_USER', 'root'); // El Usuario MySQL
+define('DB_PASS', ''); // Contraseña MySQL
+define('DB_NAME', 'gestionventas_2024'); // Nombre de la BD MySQL
+define('DB_CHARSET', 'utf8'); // Cotejamiento - Conjunto de caracteres
+?>
+
+
---
+++ api/datos.php
@@ -0,0 +1,15 @@
+<?php
+// Requerimos el archivo de la clase Modelo
+require_once('modelos.php');
+
+if(isset($_GET['tabla'])) {
+    $tabla = new ModeloABM($_GET['tabla']);
+
+    $datos = $tabla->seleccionar();
+    echo $datos;
+}
+
+
+
+
+?>
